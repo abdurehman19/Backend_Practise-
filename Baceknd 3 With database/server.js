d4a4885fcde9566099223db932ac76004b3f0b84
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-08-26 20:58:31 +0500

    Add CORS support and product model with routes
    - Integrated CORS middleware in server setup
    - Created Product model for MongoDB
    - Added product routes for fetching products
    - Updated package.json and package-lock.json to include CORS dependency

--- Baceknd 3 With database/server.js
+++ Baceknd 3 With database/server.js
@@ -1,9 +1,23 @@
-const app = require("./src/app")
-const connectdb = require("./src/db/db")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-connectdb()
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./src/db/db");
 
-app.listen(3000,()=>{
-    console.log("server is runnig on port 3000");
-    
-}) 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+ 
+connectDB()
+
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
